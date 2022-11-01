2c9644b8aa17e95bf239d9595c005ba2bc5998da
Sabrinax14 <98049249+Sabrinax14@users.noreply.github.com>
2022-11-01 21:29:16 +0000

    update

---
+++ CFG - HW/HW/cal.js
@@ -0,0 +1,41 @@
+// Add Numbers
+function Add(a,b){
+    return a + b;
+}
+const Addanswer = Add(6,2)
+console.log(Addanswer)
+
+// Multipy Numbers
+function multipy(a,b){
+    return a * b
+}
+const multipyAnswer= multipy(9,3)
+console.log(multipyAnswer)
+
+// Subtract Answer
+function Subtract(a,b){
+    return a - b
+}
+const SubtractAnswer= Subtract(14,4)
+console.log(SubtractAnswer)
+
+// Divide Numbers
+function Divide(a,b){
+    return a / b
+}
+const DivideAnswer = Divide(15,3)
+console.log(DivideAnswer)
+
+// Square Root
+function SquareNumbers(a) {
+    return Math.sqrt(a)
+}
+const SquareRoot = SquareNumbers(81)
+console.log(SquareRoot)
+
+// Power
+function Power(a,b) {
+    return Math.pow(a,b)
+}
+const PowerNumber = Power(12,2)
+console.log(PowerNumber)
